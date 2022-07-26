5117f88cf294b4c3b685250f9de564700c0a95ac
Yuvaraj Praneeth <81759881+yuvaraj-praneeth@users.noreply.github.com>
2022-07-26 21:22:41 +0530

    Update NutryUS_demo.py

--- NutryUS_demo.py
+++ NutryUS_demo.py
@@ -51,18 +51,6 @@ else:
      st.write('Input approriate height and weight')
 
 tab1, tab2, tab3 = st.tabs(["Breakfast", "Lunch", "Dinner"])
-
-with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
      
 breakfast_calories, lunch_calories, dinner_calories = 0, 0, 0
 #st.write('**You selected:**', genre)

bfa126095af37bc8de3142658b7a27fb18ff286c
Yuvaraj Praneeth <81759881+yuvaraj-praneeth@users.noreply.github.com>
2022-07-26 21:53:20 +0530

    Update NutryUS_demo.py

--- NutryUS_demo.py
+++ NutryUS_demo.py
@@ -12,7 +12,7 @@ st.set_page_config(
          'About': "# This is a header. This is an *extremely* cool app!"
      }
  )
-st.success('Successful in loading the page')
+st.success('Successfuly loaded the page! 😊')
 my_bar = st.progress(0)
 for percent_complete in range(100):
      time.sleep(0.1)
